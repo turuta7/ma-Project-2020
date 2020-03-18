79b80471117c8a9845bf05ec049e7a1d133daed7
turuta7 <turuta51@gmail.com>
2020-03-19 00:16:47 +0200

    add async/await server

--- utils/routes/users.js
+++ utils/routes/users.js
@@ -1,139 +1,73 @@
 const Router = require('@koa/router');
-const Knex = require('knex');
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const dbOptions = require('../../config/config').DB;
-const authorizationUser = require('../authorization');
 
-const knex = new Knex(dbOptions);
+const authorizationUser = require('../authorization');
+const {
+  getOneUserByKey,
+  getAllUserByKey,
+  deleteUserByKey,
+  updateUserByKey,
+  createUserDb,
+} = require('../workMethodDB');
 
 const router = new Router();
 
 const saltRounds = 10;
 
-// get email. Check password. return id, and token (id, fullname)
+// get email. Check password. return id, and token (id)
 router.post('/login', async (ctx) => {
-  const object = ctx.request.body;
-  ctx.assert(object.email && object.password, 400, 'not entered data');
-  const getUser = async () => {
-    return new Promise((resolve) => {
-      knex('users')
-        .where('email', object.email)
-        .first()
-        .then((result) => {
-          if (result === undefined) {
-            ctx.status = 403;
-            resolve((ctx.body = { message: 'no user id' }));
-          } else {
-            // decoder
-            bcrypt.compare(object.password, result.password, (err, res) => {
-              if (res) {
-                jwt.sign(
-                  { id: result.id, fullname: result.fullname },
-                  process.env.privateKeyToken,
-                  (ee, token) => {
-                    resolve((ctx.body = { user_id: result.id, token }));
-                    return null;
-                  },
-                );
-              }
-            });
-          }
-        })
-        .catch((e) => {
-          console.error(e);
-          ctx.status = 403;
-          resolve((ctx.body = { user: 'error user id' }));
-        });
-    });
-  };
-  try {
-    await getUser();
-  } catch (error) {
-    console.error(error);
+  const requestBody = ctx.request.body;
+  ctx.assert(requestBody.email && requestBody.password, 400, 'Missing login/password');
+  const user = await getOneUserByKey('email', requestBody.email, '*');
+  if (user === undefined) {
+    ctx.status = 403;
+    ctx.body = { message: 'Incorrect mail' };
+    return null;
   }
+  const isValidPassword = bcrypt.compareSync(requestBody.password, user.password);
+  const token = isValidPassword ? jwt.sign({ id: user.id }, process.env.privateKeyToken) : null;
+  ctx.status = isValidPassword ? 200 : 403;
+  ctx.body = token ? { user_id: user.id, token } : { message: 'Invalid password' };
   return null;
 });
 
-// Post user. return id and token (id, fullname)
+// Post user. return id and token (id)
 router.post('/', async (ctx) => {
-  const object = ctx.request.body;
-  ctx.assert(object.email && object.password, 400, 'not entered data');
-  const userCreate = async () => {
-    return new Promise((resolve) => {
-      bcrypt.genSalt(saltRounds, (err, salt) => {
-        bcrypt.hash(object.password, salt, (e, hash) => {
-          object.password = hash;
+  const requestBody = ctx.request.body;
 
-          knex('users')
-            .insert(object)
-            .then((result) => {
-              console.log(result);
-              knex('users')
-                .where('email', object.email)
-                .then((res) => {
-                  console.log(res[0].id);
-                  jwt.sign(
-                    { id: res[0].id, fullname: object.fullname },
-                    process.env.privateKeyToken,
-                    (ee, token) => {
-                      console.error(ee);
-                      resolve((ctx.body = { user_id: res[0].id, token }));
-                    },
-                  );
-                });
-            })
-            .catch((er) => {
-              console.error(er);
-              ctx.status = 403;
-              resolve((ctx.body = { user: 'User create error' }));
-            });
-        });
-      });
-    });
-  };
-  try {
-    await userCreate();
-  } catch (error) {
-    console.error(error);
-  }
+  ctx.assert(requestBody.email && requestBody.password, 400, 'Missing login/password');
+  const hashPassword = await bcrypt.hash(requestBody.password, saltRounds);
+  requestBody.password = hashPassword;
+  const createUser = await createUserDb(requestBody);
+  const returnCreateUser = createUser
+    ? await getOneUserByKey('email', requestBody.email)
+    : undefined;
+  const token = returnCreateUser
+    ? await jwt.sign({ id: returnCreateUser.id }, process.env.privateKeyToken)
+    : null;
+  ctx.status = returnCreateUser ? 200 : 403;
+  ctx.body = token ? { user_id: returnCreateUser.id, token } : { message: 'user in db' };
   return null;
 });
 
 // Get users
 router.get('/', async (ctx) => {
-  const getUser = async () => {
-    return new Promise((resolve) => {
-      knex('users')
-        .select([
-          'id',
-          'fullname',
-          'email',
-          'homeLatitude',
-          'homeLongitude',
-          'workLatitude',
-          'workLongitude',
-        ])
-        .then((result) => {
-          if (result === undefined) {
-            resolve((ctx.body = { message: 'no user id' }));
-          } else {
-            console.log(result);
-            resolve((ctx.body = result));
-          }
-        })
-        .catch((e) => {
-          console.error(e);
-          ctx.status = 403;
-          resolve((ctx.body = { message: 'error' }));
-        });
-    });
-  };
-  try {
-    await getUser();
-  } catch (error) {
-    console.error(error);
+  // authorization user by token and by id
+  const autUser = await authorizationUser(ctx);
+  if (!autUser.status) {
+    ctx.status = 403;
+    ctx.body = { message: 'you are not authorized' };
+    return null;
   }
+
+  const users = await getAllUserByKey();
+  if (users.length === 0) {
+    ctx.body = { message: 'no user DB' };
+    return null;
+  }
+  ctx.body = users;
   return null;
 });
 
@@ -141,6 +75,7 @@ router.get('/', async (ctx) => {
 router.get('/:id', async (ctx) => {
   const idUser = ctx.params.id;
   const id = Number(idUser);
+
   // authorization user by token and by id
   const autUser = await authorizationUser(ctx);
   if (!autUser.status || id !== autUser.id) {
@@ -149,50 +84,18 @@ router.get('/:id', async (ctx) => {
     return null;
   }
 
-  const getUserId = async () => {
-    return new Promise((resolve) => {
-      knex('users')
-        .where('id', id)
-        .select([
-          'id',
-          'fullname',
-          'email',
-          'homeLatitude',
-          'homeLongitude',
-          'workLatitude',
-          'workLongitude',
-        ])
-        .first()
-        .then((result) => {
-          if (result === undefined) {
-            ctx.status = 403;
-            resolve((ctx.body = { message: 'no user id' }));
-          } else {
-            console.log(result);
-            resolve((ctx.body = result));
-          }
-        })
-        .catch((e) => {
-          console.error(e);
-          ctx.status = 403;
-          resolve((ctx.body = { user: 'error user id' }));
-        });
-    });
-  };
-  try {
-    await getUserId();
-  } catch (error) {
-    console.error(error);
-  }
+  const user = await getOneUserByKey('id', id);
+  const returnUser = user || { message: 'user not DB' };
+  ctx.body = returnUser;
   return null;
 });
 
 // Update user by id
 router.put('/:id', async (ctx) => {
-  const object = ctx.request.body;
+  const requestBody = ctx.request.body;
   const idUser = ctx.params.id;
-  ctx.assert(object.email && object.password && idUser, 400, 'not entered data');
   const id = Number(idUser);
+
   // authorization user by token and by id
   const autUser = await authorizationUser(ctx);
   if (!autUser.status || id !== autUser.id) {
@@ -200,33 +103,16 @@ router.put('/:id', async (ctx) => {
     ctx.body = { message: 'you are not authorized' };
     return null;
   }
-  const updateUserId = async () => {
-    return new Promise((resolve) => {
-      knex('users')
-        .where('id', id)
-        .first()
-        .update(object)
-        .then((result) => {
-          if (result === 1) {
-            resolve((ctx.body = { message: 'user update ok' }));
-          } else {
-            console.log(result);
-            ctx.status = 403;
-            resolve((ctx.body = { message: 'not user id' }));
-          }
-        })
-        .catch((e) => {
-          console.error(e);
-          ctx.status = 403;
-          resolve((ctx.body = { user: 'error update id' }));
-        });
-    });
-  };
-  try {
-    await updateUserId();
-  } catch (error) {
-    console.error(error);
+  if (requestBody.password) {
+    const hashPassword = await bcrypt.hash(requestBody.password, saltRounds);
+    requestBody.password = hashPassword;
   }
+  const updateUser = await updateUserByKey('id', id, requestBody);
+  const returnUpdateUser =
+    updateUser === 1
+      ? { message: 'user update ok' }
+      : { message: 'user not DB or users email unique' };
+  ctx.body = returnUpdateUser;
   return null;
 });
 
@@ -234,6 +120,7 @@ router.put('/:id', async (ctx) => {
 router.delete('/:id', async (ctx) => {
   const idUser = ctx.params.id;
   const id = Number(idUser);
+
   // authorization user by token and by id
   const autUser = await authorizationUser(ctx);
   if (!autUser.status || id !== autUser.id) {
@@ -241,33 +128,12 @@ router.delete('/:id', async (ctx) => {
     ctx.body = { message: 'you are not authorized' };
     return null;
   }
-  const deleteUserId = async () => {
-    return new Promise((resolve) => {
-      knex('users')
-        .where('id', id)
-        .first()
-        .delete()
-        .then((result) => {
-          if (result === 1) {
-            resolve((ctx.body = { message: `user ${id} delete ok` }));
-          } else {
-            console.log(result);
-            ctx.status = 403;
-            resolve((ctx.body = { message: 'not user id' }));
-          }
-        })
-        .catch((e) => {
-          console.error(e);
-          ctx.status = 403;
-          resolve((ctx.body = { user: 'error delete id' }));
-        });
-    });
-  };
-  try {
-    await deleteUserId();
-  } catch (error) {
-    console.error(error);
-  }
+
+  const deleteUser = await deleteUserByKey('id', id);
+  const returnDeleteUser =
+    deleteUser === 1 ? { message: 'user update ok' } : { message: 'user not DB' };
+  console.log(deleteUser);
+  ctx.body = returnDeleteUser;
   return null;
 });
 
---
+++ utils/workMethodDB.js
@@ -0,0 +1,57 @@
+const Knex = require('knex');
+const dbOptions = require('../config/config').DB;
+
+const knex = new Knex(dbOptions);
+
+const objectSelect = [
+  'id',
+  'fullname',
+  'email',
+  'homeLatitude',
+  'homeLongitude',
+  'workLatitude',
+  'workLongitude',
+];
+
+const getOneUserByKey = (index, key, object = objectSelect) => {
+  return knex('users')
+    .where(`${index}`, key)
+    .select(object)
+    .first()
+    .catch((err) => console.error(err));
+};
+
+const getAllUserByKey = () => {
+  return knex('users')
+    .select(objectSelect)
+    .catch((err) => console.error(err));
+};
+
+const deleteUserByKey = (index, key) => {
+  return knex('users')
+    .where(`${index}`, key)
+    .first()
+    .delete()
+    .catch((err) => console.error(err));
+};
+const updateUserByKey = (index, key, object) => {
+  return knex('users')
+    .where(`${index}`, key)
+    .first()
+    .update(object)
+    .catch((err) => console.error(err));
+};
+
+const createUserDb = (object) => {
+  return knex('users')
+    .insert(object)
+    .catch((err) => console.error(err));
+};
+
+module.exports = {
+  getOneUserByKey,
+  getAllUserByKey,
+  deleteUserByKey,
+  updateUserByKey,
+  createUserDb,
+};
