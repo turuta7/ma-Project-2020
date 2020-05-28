99deb89e670c1837e162a32e7fa471358cb68fa7
igrantua <igrantua@gmail.com>
2020-05-28 18:50:46 +0300

    edit config

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,45 @@
 require('dotenv').config();
 
-const DB = require('../../knexfile')[process.env.NODE_ENV];
+// const DB = require('../../knexfile')[process.env.NODE_ENV];
 
 const config = {
-  DB,
+  serverPort: process.env.SERVER_PORT,
+  privateKeyToken: process.env.privateKeyToken,
+  apiKey: process.env.apiKey,
+  DB: {
+    development: {
+      client: 'postgresql',
+      connection: {
+        host: process.env.DBHOST,
+        port: process.env.DBPORT,
+        database: process.env.DBNAME,
+        user: process.env.DBUSER,
+        password: process.env.DBPASSWD,
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+      },
+    },
+    production: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user: 'username',
+        password: 'password',
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+      },
+    },
+  },
 };
 
 module.exports = config;
