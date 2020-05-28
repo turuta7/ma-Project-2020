2774422f10a08b4f792bd8205d1ce0c6bc36b6dc
igrantua <igrantua@gmail.com>
2020-05-28 10:30:33 +0300

    move getRoute logic to controllers

---
+++ src/controllers/routes.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const getRoute = async (ctx) => {
+  try {
+    const url = new URL(ctx.URL);
+    if (!url.search) {
+      ctx.status = 400;
+      ctx.body = { message: 'error URL' };
+      return null;
+    }
+    const newUrl = `https://route.ls.hereapi.com/routing/7.2/calculateroute.json${url.search}&apiKey=${process.env.apiKey}`;
+
+    const returnResponse = await axios.get(newUrl);
+    const { shape } = returnResponse.data.response.route[0];
+    const returnResult = shape.map((coordinates) =>
+      coordinates.split(',').map((point) => Number(point)),
+    );
+    const result = returnResult || { message: 'error URL' };
+    ctx.body = result;
+  } catch (error) {
+    ctx.status = 400;
+    ctx.body = { message: 'error URL' };
+  }
+  return null;
+};
+
+module.exports = getRoute;
